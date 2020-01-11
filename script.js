c7af94129c9798964c249c4700515c7f121bb0a0
Adrian Romero <romeroaadrian@gmail.com>
2020-01-10 20:15:16 -0500

    third commit

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 $(document).ready(function () {
     // var cityId = "5131095"
+    var currentDayHTML = document.getElementsByClassName("current-day");
 
 
-    function getWeather(city) {
+    function forecast(city) {
 
-        var APIkey = "b6907d289e10d714a6e88b30761fae22";
+        var APIkey = "6f0f594690b75a6bba0cc5af02c91545";
 
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=b6907d289e10d714a6e88b30761fae22&units=imperial";
+        var queryURL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APIkey}&units=metric`;
         
 
-        $.ajax({
+        return $.ajax({
             type: "GET",
             url: queryURL,
-            dataType: 'jsonp',
-            success: function(data){
-                debugger
-            }
+            timeout: 0,
+            headers:{
+                "content-type": "application/json"
+            }, 
+            success: function(){},
+            failure: function(){}
         })
     }
 
-    getWeather("philadelphia");
+    function setHTML (days){
+        for (var i = 0; i < days.length; i = i + 8){
+            console.log(days[i]);
+            var correctDate = new Date(days[i].dt_txt);
+            correctDate.setHours(correctDate.getHours() - 5);
+            if(i == 0){
+                for(let day of currentDayHTML){
+                    if(day.id == "c_title") day.textContent = correctDate.toLocaleDateString();
+                    else if(day.id == "c_temp") day.textContent = days[i].main.temp;
+                    else if(day.id == "c_humidity") day.textContent = days[i].main.humidity;
+                    else if(day.id == "c_wind") day.textContent = days[i].wind.speed;
+                }
+            }
 
-    // debugger
-    // localStorage.setItem("dog", "max");
+            if(i == 32) i--;
+        }
+    }
+    forecast("Philadelphia").then( function(result){
+        //console.log(result);
+        setHTML(result.list);
+    });
+    
+
+
 
     // debugger
     // var dog = localStorage.getItem("dog")
@@ -30,6 +53,4 @@ $(document).ready(function () {
 
 
  
-    
-
 })
